04a9759fc56db80451c976a7108a4266d1d4baf8
rim sassi <rim@wealzy.com>
2022-07-07 16:11:26 +0100

    rec_sys

---
+++ app_flask.py
@@ -0,0 +1,33 @@
+import flask
+import pandas as pd
+
+
+df_output = pd.read_csv('C:/Users/rim/Desktop/sys_rec/option1_recommendation_new.csv')
+
+
+def customer_recomendation(customer_id):
+    if customer_id not in df_output.index:
+        print('Customer not found.')
+        return customer_id
+    return df_output.loc[customer_id]
+
+app = flask.Flask(__name__, template_folder='template')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if flask.request.method == 'GET':
+        return (flask.render_template('main.html'))
+
+    if flask.request.method == 'POST':
+        customer_id = flask.request.form['customer_id']
+        customer_id = int(customer_id)
+        recommendedProducts = customer_recomendation(customer_id)
+
+        return flask.render_template('main.html', original_input={'Identifiant du client': 
+            customer_id},result=recommendedProducts.iloc[1])
+
+
+if __name__ == '__main__':
+    #app.debug = True
+    app.run()
